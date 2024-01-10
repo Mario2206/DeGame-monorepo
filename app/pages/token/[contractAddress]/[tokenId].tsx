be53b5b5aec6656315b19311840e87df2779e5ca
EL MOBACHI Adil <adilelmoba20@gmail.com>
2024-01-10 09:06:03 +0100

    feat: gamepage (#4)

--- app/pages/token/[contractAddress]/[tokenId].tsx
+++ app/pages/token/[contractAddress]/[tokenId].tsx
@@ -1,65 +1,103 @@
-import { ThirdwebNftMedia } from "@thirdweb-dev/react";
-import React from "react";
-import Container from "../../../components/Container/Container";
-import { GetStaticProps, GetStaticPaths } from "next";
-import { NFT_COLLECTION_ADDRESS } from "../../../const/contractAddresses";
-import styles from "../../../styles/Token.module.css";
-import { Toaster } from "react-hot-toast";
-import { NftGame } from "../../../util/types";
-import { getAllMintableNfts, hasGame } from "../../../util/wallet";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { Toaster } from "react-hot-toast";
+import Container from "../../../components/Container/Container";
+import { NftGame } from "../../../util/types";
+import { getAllMintableNfts } from "../../../util/wallet";
+import styles from "../../../styles/Token.module.css";
 
-type Props = {
-	nft: NftGame;
-	contractMetadata: any;
+interface StarRatingProps {
+  rating: number;
+  setRating: (rating: number) => void;
+  editable: boolean;
+}
+
+const StarRating: React.FC<StarRatingProps> = ({
+  rating,
+  setRating,
+  editable,
+}) => {
+  return (
+    <div className={styles.starRating}>
+      {Array.from({ length: 5 }, (_, index) => (
+        <span
+          key={index}
+          className={rating > index ? styles.star : ""}
+          onClick={() => editable && setRating(index + 1)}
+        >
+          {rating > index ? "★" : "☆"}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default function TokenPage() {
-	const router = useRouter();
+  const router = useRouter();
+  const { tokenId } = router.query;
 
-	const { tokenId } = router.query;
+  const [nft, setNft] = useState<NftGame | undefined>(undefined);
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(0);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
 
-	const [nft, setNft] = React.useState<NftGame | undefined>(undefined);
+  const handleCommentSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setHasSubmitted(true); 
+  };
 
-	React.useEffect(() => {
-		getAllMintableNfts().then(async (nfts) => {
-			const nft = nfts.find((nft) => nft.id === Number(tokenId));
-			if (!nft) return;
+  useEffect(() => {
+    getAllMintableNfts().then(async (nfts) => {
+      const game = nfts.find((game) => game.id === Number(tokenId));
+      if (game) {
+        setNft(game);
+      } else {
+        router.push("/games");
+      }
+    });
+  }, [tokenId, router]);
 
-			if (await hasGame(nft.id)) {
-				setNft(nft);
-        return
-			}
-      router.push("/games");
-		});
-	}, [tokenId]);
+  if (!nft) {
+    return <p>Loading...</p>;
+  }
 
-	if (!nft) {
-		return <p>Loading...</p>;
-	}
-
-	return (
-		<>
-			<Toaster position="bottom-center" reverseOrder={false} />
-			<Container maxWidth="lg">
-				<div className={styles.container}>
-					<div
-						style={{
-							width: "100vw",
-							height: "100vh",
-							position: "absolute",
-							top: 0,
-							left: 0,
-							display: "flex",
-							justifyContent: "center",
-							alignItems: "center",
-						}}
-					>
-						<span style={{ marginRight: "8px" }}>Play to</span>{" "}
-						<strong>{nft.name}</strong>
-					</div>
-				</div>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <Container maxWidth="lg">
+        <div className={styles.container}>
+          <div className={styles.containerGame}>
+            <div className={styles.gameInfo}>
+              <span>Play to</span>
+              <h1 className={styles.gameTitle}>{nft.name}</h1>
+            </div>
+            <img src={nft.image} alt={nft.name} className={styles.gameImage} />
+            <div>
+              <form
+                onSubmit={handleCommentSubmit}
+                className={styles.commentForm}
+              >
+                <StarRating
+                  rating={rating}
+                  setRating={setRating}
+                  editable={!hasSubmitted}
+                />
+                <textarea
+                  className={styles.commentTextarea}
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  placeholder="Write your comment here..."
+                  rows={4}
+                  disabled={hasSubmitted}
+                />
+                <button className={styles.submitButton} type="submit">
+                  {hasSubmitted ? "Update your vote?" : "Submit Comment"}
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
