16da0831a6044a907a6bcc3a1b2644963380fcc5
Mathieu <mathieu.raimbault@hotmail.com>
2024-01-11 14:17:29 +0100

    test: game collection

--- test/BadgeManager.ts
+++ test/BadgeManager.ts
@@ -2,7 +2,6 @@ import {
   time,
   loadFixture,
 } from '@nomicfoundation/hardhat-toolbox/network-helpers';
-import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
